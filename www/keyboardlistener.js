b2c0a6dcb20a9880baa963520b158155eca0914c
gregshutt <github@gregshutt.com>
2016-09-06 13:38:56 -0400

    Update keyboardlistener.js

--- www/keyboardlistener.js
+++ www/keyboardlistener.js
@@ -5,9 +5,28 @@ var KeyboardListener = {
     exec(
       function(key) {
         if(key !== "") {
+          // dispatch a custom keyboard listener event
           var e = new CustomEvent('keyboard-listener-keypress', { detail: { 'keycode': key }} );
           document.dispatchEvent(e);
-        }      
+
+          // also simulate a keypress, since the ios plugin eats up the keys
+          // yanked from http://stackoverflow.com/questions/596481/simulate-javascript-key-events
+          e = document.createEvent("KeyboardEvent");
+          var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
+          e[initMethod](
+                   "keydown", // event type : keydown, keyup, keypress
+                    true, // bubbles
+                    true, // cancelable
+                    window, // viewArg: should be window
+                    false, // ctrlKeyArg
+                    false, // altKeyArg
+                    false, // shiftKeyArg
+                    false, // metaKeyArg
+                    key, // keyCodeArg : unsigned long the virtual key code, else 0
+                    0 // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
+          );
+          document.dispatchEvent(e);
+        }
       },
 
       function() {
